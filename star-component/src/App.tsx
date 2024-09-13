991025f65d1a195c099e2a13460dadf094f654ce
khanidrees43 <idreeskhan1234@gmail.com>
2024-09-13 12:07:59 +0530

    star component added

---
+++ star-component/src/App.tsx
@@ -0,0 +1,10 @@
+import StarComponent from "./components/StarComponent";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <StarComponent />
+    </div>
+  );
+}
